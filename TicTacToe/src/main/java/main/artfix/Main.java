2e6ca8fcedb3db68c5494d3ec0c6fcaac972f11f
Arthur <125185500+ArtFixProg@users.noreply.github.com>
2023-03-20 18:32:43 +0400

    Minor changes

--- TicTacToe/src/main/java/main/artfix/Main.java
+++ TicTacToe/src/main/java/main/artfix/Main.java
@@ -2,16 +2,7 @@ package main.artfix;
 
 public class Main {
     public static void main(String[] args) {
-        FileIng.read("banner");
-        String ApplicationBanner = FileIng.ReadData;
-
-        if (ApplicationBanner.equals("true")) {
-            System.out.println("Support projects https://ko-fi.com/artfixprog.");
             Application.start();
-        } else {
-            Application.start();
-        }
-
 
     }
 }
